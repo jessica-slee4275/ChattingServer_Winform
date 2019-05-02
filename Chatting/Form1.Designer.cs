346317611f81b04eb68617c6c7ae5e81de64f79d
jessica-slee4275 <jessica.slee4275@gmail.com>
2019-05-01 21:42:13 -0400

    chatting server commit

---
+++ Chatting/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Chatting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnServer = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.lbChatHistory = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnServer
+            // 
+            this.btnServer.Location = new System.Drawing.Point(424, 12);
+            this.btnServer.Name = "btnServer";
+            this.btnServer.Size = new System.Drawing.Size(75, 69);
+            this.btnServer.TabIndex = 1;
+            this.btnServer.Text = "Server";
+            this.btnServer.UseVisualStyleBackColor = true;
+            this.btnServer.Click += new System.EventHandler(this.btnServer_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(424, 254);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 69);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(12, 254);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(406, 68);
+            this.tbText.TabIndex = 3;
+            // 
+            // lbChatHistory
+            // 
+            this.lbChatHistory.FormattingEnabled = true;
+            this.lbChatHistory.Location = new System.Drawing.Point(13, 12);
+            this.lbChatHistory.Name = "lbChatHistory";
+            this.lbChatHistory.Size = new System.Drawing.Size(405, 238);
+            this.lbChatHistory.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 335);
+            this.Controls.Add(this.lbChatHistory);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnServer);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnServer;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.ListBox lbChatHistory;
+    }
+}
+
---
+++ Chatting/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Chatting
+{
+    public partial class Form1 : Form
+    {
+        Socket listen_socket;
+        Socket client_socket;
+        bool isConnected; //server and client
+
+        byte[] bytes = new byte[1024];
+        string data; //chat data
+        public Form1()
+        {
+            InitializeComponent();
+            isConnected = false; //initialize connection
+        }
+
+        private void btnServer_Click(object sender, EventArgs e)
+        {
+            start("127.0.0.1", 7777, 10);
+        }
+
+        public void start(string host, int port, int backlog) {
+            this.listen_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress address;
+            if (host == "0.0.0.0"){
+                address = IPAddress.Any;
+            }
+            else {
+                address = IPAddress.Parse(host);
+            }
+            IPEndPoint endPoint = new IPEndPoint(address, port);
+            try
+            {
+                listen_socket.Bind(endPoint);
+                listen_socket.Listen(backlog);
+
+                client_socket = listen_socket.Accept();
+                lbChatHistory.Items.Add("Start Connection...");
+                isConnected = true;
+                Thread listen_thread = new Thread(do_receive);
+                listen_thread.Start();
+            }
+            catch (Exception e) { }
+        }
+        void do_receive() {
+            while (isConnected) {
+                while (true) {
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = client_socket.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<eof>") > -1)
+                        break;
+                }
+                data = data.Substring(0, data.Length - 5);
+                Invoke((MethodInvoker)delegate {
+                    lbChatHistory.Items.Add(data);
+                });
+                data = "";
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (isConnected == false)
+                return;
+            byte[] msg = Encoding.UTF8.GetBytes(tbText.Text + "<eof>");
+            int bytesSent = client_socket.Send(msg);
+            tbText.Clear();
+            tbText.Text = "";
+        }
+    }
+}
